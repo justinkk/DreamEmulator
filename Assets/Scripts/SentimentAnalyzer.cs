d650f90f755c2faa8c39eb356d6abaef4b2586b1
Justin Krasner-Karpen <jkrasner.karpen@gmail.com>
2018-09-25 23:04:42 -0700

    created git repository. can read text from player, and from sentiment tsv

---
+++ Assets/Scripts/SentimentAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SentimentAnalyzer : MonoBehaviour {
+  private static string FILENAME = "Assets/TSVs/AFINN/AFINN-111.txt";
+
+  private static Dictionary<string, int> sentimentMap;
+
+	// Use this for initialization
+	void Start () {
+    try {
+      using (StreamReader reader = new StreamReader(FILENAME)) {
+        sentimentMap = new Dictionary<string, int>();
+
+        string line;
+
+        while ((line = reader.ReadLine()) != null) {
+          string[] splitLine = line.Split('\t');
+          sentimentMap.Add(splitLine[0], System.Int32.Parse(splitLine[1]));
+        }
+      }
+    } catch (System.Exception e) {
+      Debug.Log("Could not read file.");
+      Debug.Log(e.Message);
+    }
+	}
+
+  void ProcessText(string text) {
+    //To lowercase
+
+//    string currentWord = "";
+//    for (int i = 0; i < text.Length(); i++) {
+
+        //if alphabetic, add to word
+        //else,
+           //end previous word
+           //if previous word is in dictionary
+              //do something with the number result.
+              //(temporary, debug.log word and value.)
+              //(later, add it to a queue of the last few words, which we will average somehow
+
+    // this'll eventually be different for the final version of submitted text, compared
+    // to what it looks like as the game is going.
+          
+//    }
+  }
+}
---
+++ Assets/Scripts/TextInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(SentimentAnalyzer))]
+[RequireComponent(typeof(InputField))]
+public class TextInput : MonoBehaviour {
+  /** Processor for the text. */
+  SentimentAnalyzer sentimentAnalyzer;
+
+  /** Handler for text input. */
+  InputField inputField;
+
+	// Use this for initialization
+	void Start () {
+    sentimentAnalyzer = GetComponent<SentimentAnalyzer>();
+    inputField = GetComponent<InputField>();
+
+    // Add a listener for text changing.
+    inputField.onValueChanged.AddListener(delegate {OnValueChanged(); });
+
+    // TODO listen for EndEdit or submit event?
+	}
+
+  public void OnValueChanged() {
+    Debug.Log(inputField.text);
+  }
+}
